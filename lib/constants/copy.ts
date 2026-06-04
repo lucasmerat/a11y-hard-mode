c772e34c911d046f519d4d13f2a333b3e949f32d
Lucas Merat <lucas.merat@gmail.com>
2026-06-04 13:11:57 +0200

    Move game HUD to levels index route and specify it to active level

---
+++ lib/constants/copy.ts
@@ -0,0 +1,11 @@
+export const frictionEventCopy = {
+  level1: "Misclicks",
+  level2: "Incorrect form submissions",
+  level3: "Failed save attempts",
+} as const
+
+export const levelCopy = {
+  level1: "Level 1",
+  level2: "Level 2",
+  level3: "Level 3",
+} as const
